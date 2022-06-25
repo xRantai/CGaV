09b0ddf8dcce063db9f1fa7ef74f43c4a5c045f7
Rantai <blazej.orzechowski@student.put.poznan.pl>
2022-06-25 20:47:18 +0200

    Revert "Shading model WIP"
    This reverts commit 4ae3e23ce222207ca9968acf50927caa6477c26b.

--- model.cpp
+++ model.cpp
@@ -1,17 +1,13 @@
 #include "model.h"
 
-
-
 Model::Model() {}
 
-Model::Model(std::string plik, const char* texture, BoundTypes boundType) 
-	: boundType(boundType) {
+Model::Model(std::string plik, const char* texture)  {
 	Assimp::Importer importer;
 	std::vector< glm::vec4 > vertices;
 	std::vector< glm::vec2 > texCoords;
 	std::vector< glm::vec4 > normals;
 	std::vector<unsigned int> indices;
-	this->material = Material::emerald;
 
 	const aiScene* scene = importer.ReadFile(plik, aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_GenSmoothNormals);
 	std::cout << importer.GetErrorString() << std::endl;
